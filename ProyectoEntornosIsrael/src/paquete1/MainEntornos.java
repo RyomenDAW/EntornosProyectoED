6b7c2357133e79b175bb253dd16b2aeb2ca9307c
alumnado <alumnado@A120-PC01>
2024-03-12 13:24:32 +0100

    La rama aun no se creo, se creara ahora, primer commit.

---
+++ ProyectoEntornosIsrael/src/paquete1/MainEntornos.java
@@ -0,0 +1,11 @@
+package paquete1;
+
+public class MainEntornos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Este proyecto fue realizado por Israel Txakon");
+		System.out.println("Courtesy Call");
+	}
+
+}
