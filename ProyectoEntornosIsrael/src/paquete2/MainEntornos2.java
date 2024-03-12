13784e83c63dda5b5999d2a854b45b0be9bd4d20
alumnado <alumnado@A120-PC01>
2024-03-12 13:50:55 +0100

    13:50

---
+++ ProyectoEntornosIsrael/src/paquete2/MainEntornos2.java
@@ -0,0 +1,13 @@
+package paquete2;
+
+public class MainEntornos2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Este proyecto fue realizado por Israel Txakon");
+		System.out.println("Slowed Reverb");
+		
+
+	}
+
+}
